3ed638dd451eb4424b7b5ba5276affc3f7ca8851
khancerberus <khancerberus@hotmail.cl>
2021-10-04 22:01:19 -0300

    Se crean los modelos

--- caso3project/reg_reservas/models.py
+++ caso3project/reg_reservas/models.py
@@ -6,10 +6,15 @@ class Cliente(models.Model):
     nombre = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
 
-# Clase incompleta
 class Vehiculo(models.Model):
     patente = models.CharField(max_length=6, primary_key=True)
     marca = models.CharField(max_length=50)
     modelo = models.CharField(max_length=50)
     anio = models.IntegerField()
     
+class Reserva(models.Model):
+    id = models.AutoField(primary_key=True)
+    motivo = models.CharField(max_length=100)
+    fecha_hora = models.DateTimeField()
+    cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    vehiculo = models.ForeignKey(Vehiculo, on_delete=models.CASCADE)
