b01dc6ef57492a5f44400aa131f8c108adfe2105
khancerberus <khancerberus@hotmail.cl>
2021-10-04 22:22:32 -0300

    Se agregan los modelos al administrador

--- caso3project/reg_reservas/admin.py
+++ caso3project/reg_reservas/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Vehiculo, Cliente, Reserva
+
 # Register your models here.
+@admin.register(Vehiculo)
+class VehiculoAdmin(admin.ModelAdmin):
+    fields = ('patente', 'marca', 'modelo', 'anio')
+
+@admin.register(Cliente)
+class Cliente(admin.ModelAdmin):
+    fields = ('rut', 'nombre', 'apellidos')
+
+@admin.register(Reserva)
+class Reserva(admin.ModelAdmin):
+    fields = ('id', 'motivo', 'fecha_hora')
+
