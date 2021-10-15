343b4be4d23f175e1a23b7bc1ff7d25e7208a4fa
khancerberus <khancerberus@hotmail.cl>
2021-10-15 18:23:41 -0300

    Se agregan list_display al admin

--- caso3project/reg_reservas/admin.py
+++ caso3project/reg_reservas/admin.py
@@ -6,12 +6,15 @@ from .models import Vehiculo, Cliente, Reserva
 @admin.register(Vehiculo)
 class VehiculoAdmin(admin.ModelAdmin):
     fields = ('patente', 'marca', 'modelo', 'anio')
+    list_display = ('patente', 'marca', 'modelo', 'anio')
 
 @admin.register(Cliente)
 class Cliente(admin.ModelAdmin):
     fields = ('rut', 'nombre', 'apellidos')
+    list_display = ('rut', 'nombre', 'apellidos')
 
 @admin.register(Reserva)
 class Reserva(admin.ModelAdmin):
     fields = ('id', 'motivo', 'fecha_hora')
+    list_display = ('id', 'motivo', 'fecha_hora')
 
